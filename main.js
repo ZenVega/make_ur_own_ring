133208d8cfe80c82846b977b25aa607ec99f2fa2
ZenVega <50147356+ZenVega@users.noreply.github.com>
2020-08-14 10:11:36 +0200

    bug fixes

--- main.js
+++ main.js
@@ -20,60 +20,61 @@ descriptions[2] = document.querySelector("#d2");
 descriptions[3] = document.querySelector("#d3");
 descriptions[4] = document.querySelector("#d4");
 
+if(window.innerWidth <= 850){
 
-const goToSlide = (container, slideNumber) => {
-  container.style.transform = "translateX(-" + (slideNumber-1)*92 + "vw)";
-  if(container === slideContainer){
-    descriptions.map(description => description.style.display = "none");
-    descriptions[slideNumber].style.display = "block";
+  const goToSlide = (container, slideNumber) => {
+    container.style.transform = "translateX(-" + (slideNumber-1)*92 + "vw)";
+    if(container === slideContainer){
+      descriptions.map(description => description.style.display = "none");
+      descriptions[slideNumber].style.display = "block";
+    }
   }
-}
-//SLIDER HOW
-
-let currentSlide = 1;
-const slider = document.querySelector(".slider");
-const slideContainer = document.querySelector('.how-images');
-
-//swipe left
-slider.addEventListener('swiped-left', () => {
-  swipeTag.style.display = 'none';
-  if(currentSlide == 4){
-    return;
-  } else {
-    currentSlide++;
-  }
-  goToSlide(slideContainer, currentSlide);
-})  
-
-slider.addEventListener('click', () => {
-  swipeTag.style.display = 'none';
-  if(currentSlide == 4){
-    return;
-  } else {
-    currentSlide++;
-  }
-  goToSlide(slideContainer, currentSlide);
-})  
-
-//swipe-right
-slider.addEventListener('swiped-right', () => {
-  if(currentSlide == 1){
-    return;
-  } else {
-    currentSlide--;
-  }
-  goToSlide(slideContainer, currentSlide);
-})
-
-
-//SLIDER EXAMPLES
-
-let currentExampleSlide = 1;
-const sliderExamples = document.querySelector(".slider-examples");
-const slideContainerExamples = document.querySelector('.about-images');
-
-//swipe left
-sliderExamples.addEventListener('swiped-left', () => {
+  //SLIDER HOW
+  
+  let currentSlide = 1;
+  const slider = document.querySelector(".slider");
+  const slideContainer = document.querySelector('.how-images');
+  
+  //swipe left
+  slider.addEventListener('swiped-left', () => {
+    swipeTag.style.display = 'none';
+    if(currentSlide == 4){
+      return;
+    } else {
+      currentSlide++;
+    }
+    goToSlide(slideContainer, currentSlide);
+  })  
+  
+  slider.addEventListener('click', () => {
+    swipeTag.style.display = 'none';
+    if(currentSlide == 4){
+      return;
+    } else {
+      currentSlide++;
+    }
+    goToSlide(slideContainer, currentSlide);
+  })  
+  
+  //swipe-right
+  slider.addEventListener('swiped-right', () => {
+    if(currentSlide == 1){
+      return;
+    } else {
+      currentSlide--;
+    }
+    goToSlide(slideContainer, currentSlide);
+  })
+  
+  
+  //SLIDER EXAMPLES
+  
+  let currentExampleSlide = 1;
+  const sliderExamples = document.querySelector(".slider-examples");
+  const slideContainerExamples = document.querySelector('.about-images');
+  
+  //swipe left
+  sliderExamples.addEventListener('swiped-left', () => {
     swipeTagExample.style.display = 'none';
     if(currentExampleSlide == 9){
       return;
@@ -92,9 +93,9 @@ sliderExamples.addEventListener('swiped-left', () => {
     }
     goToSlide(slideContainerExamples, currentExampleSlide);
   })  
-
-//swipe-right
-sliderExamples.addEventListener('swiped-right', () => {
+  
+  //swipe-right
+  sliderExamples.addEventListener('swiped-right', () => {
     if(currentExampleSlide == 1){
       return;
     } else {
@@ -102,5 +103,7 @@ sliderExamples.addEventListener('swiped-right', () => {
     }
     goToSlide(slideContainerExamples, currentExampleSlide);
   })
-
-
+  
+  
+  
+}
